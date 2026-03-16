9de1201d3dda95566c49925aabbbf04c52f4e05f
Jeff <jdanhutch@gmail.com>
2026-03-15 18:31:36 -0600

    Acceptance test suite with Selenium

--- tests/Web/Person/PersonChoresPageCest.php
+++ tests/Web/Person/PersonChoresPageCest.php
@@ -52,32 +52,4 @@ final class PersonChoresPageCest
         $I->click('John Doe');
         $I->seeCurrentUrlEquals('/person-admin/019cd5cd-8ba6-723d-8525-01672c6a37b6');
     }
-
-    public function doneButton(WebTester $I): void
-    {
-        $I->wantTo('person chores page\'s done button works');
-        $I->amOnPage('/person-admin/019cd5cd-8ba6-723d-8525-01672c6a37b6/chores');
-        $I->expectTo('update the database record and stay on the page');
-
-        $I->seeInDatabase('public.chore', ['id' => '019cd5cd-d2d8-72a9-b4c3-41fef1019587', 'person_id' => '019cd5cd-8ba6-723d-8525-01672c6a37b6', 'done' => false]);
-        $I->seeInDatabase('public.chore', ['id' => '019cd5ce-0b7c-7373-8514-256dad0fe4da', 'person_id' => '019cd5cd-8ba6-723d-8525-01672c6a37b6', 'done' => false]);
-        $I->see('', 'tr:nth-child(1) .chore-checkbox:not(checked)'); // chore 1 is not done
-        $I->see('', 'tr:nth-child(2) .chore-checkbox:not(checked)'); // chore 2 is not done
-
-        // Mark chore done
-        $I->click('tr:nth-child(1) .chore-checkbox');
-        $I->seeCurrentUrlEquals('/person-admin/019cd5cd-8ba6-723d-8525-01672c6a37b6/chores');
-        $I->seeInDatabase('public.chore', ['id' => '019cd5cd-d2d8-72a9-b4c3-41fef1019587', 'person_id' => '019cd5cd-8ba6-723d-8525-01672c6a37b6', 'done' => true]);
-        $I->seeInDatabase('public.chore', ['id' => '019cd5ce-0b7c-7373-8514-256dad0fe4da', 'person_id' => '019cd5cd-8ba6-723d-8525-01672c6a37b6', 'done' => false]);
-        $I->see('', 'tr:nth-child(1) .chore-checkbox:checked'); // chore 1 is done
-        $I->see('', 'tr:nth-child(2) .chore-checkbox:not(checked)'); // chore 2 is not done
-
-        // Mark chore not done
-        $I->click('tr:nth-child(1) .chore-checkbox');
-        $I->seeCurrentUrlEquals('/person-admin/019cd5cd-8ba6-723d-8525-01672c6a37b6/chores');
-        $I->seeInDatabase('public.chore', ['id' => '019cd5cd-d2d8-72a9-b4c3-41fef1019587', 'person_id' => '019cd5cd-8ba6-723d-8525-01672c6a37b6', 'done' => false]);
-        $I->seeInDatabase('public.chore', ['id' => '019cd5ce-0b7c-7373-8514-256dad0fe4da', 'person_id' => '019cd5cd-8ba6-723d-8525-01672c6a37b6', 'done' => false]);
-        $I->see('', 'tr:nth-child(1) .chore-checkbox:not(checked)'); // chore 1 is not done
-        $I->see('', 'tr:nth-child(2) .chore-checkbox:not(checked)'); // chore 2 is not done
-    }
 }
